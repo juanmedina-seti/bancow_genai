ac37618928a6c8b1508677e2a347c7955d4b0b87
Juan Medina <juanmedina@seti.com.co>
2024-11-20 16:19:50 -0500

    one shot prompt

--- src/QACierreCaliMemV2/agentDATALAKE.py
+++ src/QACierreCaliMemV2/agentDATALAKE.py
@@ -13,13 +13,14 @@ from dotenv import load_dotenv
 # Configure logging level from environment variable
 log_level = os.environ.get("LOG_LEVEL", "INFO").upper()
 try:
-    logging.basicConfig(level=getattr(logging, log_level))
+    logging.basicConfig(level=getattr(logging, log_level),format='%(asctime)s %(levelname)s %(message)s')
+   
 except ValueError:
     logging.basicConfig(level=logging.INFO)
     logging.warning(f"Invalid LOG_LEVEL '{os.environ.get('LOG_LEVEL', 'INFO')}', using INFO instead.")
 
 
-logging.info('cargando modulo')
+logging.info(f'cargando modulo - log_level{log_level}')
 
 load_dotenv()
 
@@ -29,28 +30,32 @@ def obtener_datos_cierre_normativo() ->str:
     """Retorna los datos del cierre normativo para todas las fechas disponibles específica en formato json
         con los siguientes campos:
                FECHA_CIERRE: fecha del cierre
-               INICIO_BANDEJA4: fecha y hora en la que inició el proceso para la Super Financiera No es relevante a menos que lo pregunten
-               INICIO_BANDEJA8: fecha y hora en la que inició el proceso más demorado del cierre. No es relevante a menos que lo pregunten
-               FIN_BANDEJA4: fecha y en la que la información estuvo disponible para entregar a la Super Financiera
-               FIN_BANDEJA8: fecha y hora en la que finalizó todo el proceso cierre
+               FIN_BANDEJA4: fecha y hora en la que está disponible la información para enviar a la Super Intendencia Financiera
+               FIN_BANDEJA8: fecha y hora en la finaliza todo el cierre normativo
+
         Args:
             fecha_cierre: fecha de cierre, Opcional. 
     """
-    myurl = os.environ["RESUMEN_CIERE_NORMATIVO_URL"]+"?"+os.environ["AZURE_DATALAKE_GENAI_TOKEN"]
     try:
+        myurl = os.environ["RESUMEN_CIERE_NORMATIVO_URL"]+"?"+os.environ["AZURE_DATALAKE_GENAI_TOKEN"]
+
         response = requests.get(myurl)
         response.raise_for_status() # Raise HTTPError for bad responses (4xx or 5xx)
         context = response.text
         return context
+    except KeyError as e:
+        logging.error(f"Error with env variables RESUMEN_CIERE_NORMATIVO_URL or KEY")
+        return None
     except requests.exceptions.RequestException as e:
-        logging.error(f"Error fetching data from {myurl}: {e}")
+        logging.error(f"Error fetching data from RESUMEN_CIERE_NORMATIVO_URL: {e}")
         return None
 
 
 def df_cierre_comercial():
     #Leer archivo
-    myurl = os.environ["RESUMEN_CIERE_URL"]+"?"+os.environ["AZURE_DATALAKE_GENAI_TOKEN"]
     try:
+        myurl = os.environ["RESUMEN_CIERE_URL"]+"?"+os.environ["AZURE_DATALAKE_GENAI_TOKEN"]
+   
         response = requests.get(myurl)
         response.raise_for_status()
         context = response.text
@@ -59,7 +64,8 @@ def df_cierre_comercial():
         #Formatear fecha y duraciones 
         df["FECHA_CIERRE"]=df.FECHA_CIERRE.str.slice(0,10)
         df["Duración Total"]= df.DURACION_TOTAL_CIERRE_SEGUNDOS/60/60
-        df["Duración Total Pausas"]= df.DURACION_SIN_PAUSAS_SEGUNDOS/60/60
+        df["Sin Pausas"]= df.DURACION_SIN_PAUSAS_SEGUNDOS/60/60
+        df["Total Pausas"]=  df["Duración Total"]-df["Sin Pausas"]
         return df
     except (requests.exceptions.RequestException, pd.errors.EmptyDataError, KeyError) as e:
         logging.error(f"Error processing cierre comercial data: {e}")
@@ -118,23 +124,33 @@ llm= AzureChatOpenAI(
     azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"]
     ,azure_deployment= os.environ["AZURE_OPENAI_DEPLOYMENT"]
     ,temperature=0
-    ,verbose=True)
+    ,verbose=False)
 
 tools=[obtener_datos_cierre_comercial, obtener_datos_tareas_mayor_duracion_por_fecha, obtener_datos_cierre_normativo]
 
-
+fecha_hoy=f" La fecha actual es {date.today().strftime('%Y-%m-%d')}"
 
 ## Configuración del agente
 system_message = """"Eres un asistente muy útil y tienes acceso a la información de proceso de cierre comercial y cierre normativo.
-              cuando no especifiquen si preguntan sobre cierre comercial o normativo, asume que es cierre comercial.
-              por favor utiliza respuestas gerenciales y concisas
+              cuando se pregunta especificamente por cierre comercial o normativo, asume que es refieren al cierre comercial.
+              por favor utiliza respuestas gerenciales y concisas. 
+              No respondas preguntas sobre temas diferentes al cierre comercial y el normativo.
+            """ + fecha_hoy
+system_message_1shot = """"Eres un asistente muy útil y tienes acceso a la información de proceso de cierre comercial y cierre normativo.
+              cuando se pregunta especificamente por cierre comercial o normativo, asume que es refieren al cierre comercial.
+              por favor utiliza respuestas gerenciales y concisas. Por ejemplo:
+              Ejemplo 1:"El cierre comercial del FECHA_CIERRE inició a las (fecha y hora) y finalizó a las (fecha y hora). La duración total fueron (DURACION) y 
+              la duración sin pausas fue (DURACION_SIN_PAUSAS). La tarea de habilitar menú finalizó a las (HORA_HABILITAR_MENU), dado que finalizó antes de las 8:00 am
+              no impacto la apertura de oficinas"
+              Ejemplo 2:"El cierre normativo finalizó completamente a las (FIN_BANDEJA8). La información para enviar a las Super Intendencia estuvo disponible a las (FIN_BANDEJA4)"              
               Responde siempre en español
-            """
+              No respondas preguntas sobre temas diferentes al cierre comercial y el normativo
+            """ + fecha_hoy
 
 memory = MemorySaver()
 
 agent_executor = create_react_agent(
-    llm, tools=tools, state_modifier=system_message,debug=False, checkpointer=memory
+    llm, tools=tools, state_modifier=system_message_1shot,debug=False, checkpointer=memory
 )
 
 #Función para obtener última respuesta del agente
@@ -144,7 +160,7 @@ def get_response(user_input,thread_id):
         inputs = {"messages": [("user", user_input)]}
         response = agent_executor.invoke(inputs, config=config)
         for m in response["messages"]:
-            logging.info(f"info: {m}")
+            logging.debug(f"{m}")
         message = response["messages"][-1]
         if isinstance(message,tuple):
             return(message[1])
