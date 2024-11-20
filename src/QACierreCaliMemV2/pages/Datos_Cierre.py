5481677476aab25291414a9fc90fb86e029aa43d
Juan Medina <juanmedina@seti.com.co>
2024-11-20 06:27:36 -0500

    groupby

--- src/QACierreCaliMemV2/pages/Datos_Cierre.py
+++ src/QACierreCaliMemV2/pages/Datos_Cierre.py
@@ -22,7 +22,7 @@ if "thread_id" not in st.session_state:
 # Configuración de la aplicación
 st.title("Consulta Cierre 30 últimos días")
 st.divider()
-col1, col2 = st.columns([0.4,0.6])
+col1, col2 = st.columns([0.4,0.6],gap='medium')
 # Mostrar el historial de la conversación
 def display_chat(history: ChatMessageHistory):
     for message in history.messages:
@@ -39,23 +39,26 @@ with col1:
     st.bar_chart(df,x="FECHA_CIERRE",y=["Duración Total","Duración Total Pausas"],stack=False, y_label="Duración (hrs)")
 
 with col2:
-    display_chat(st.session_state.chat_memory)
+    chat_container = st.container(height=500)
+    with chat_container:
+        display_chat(st.session_state.chat_memory)
 
     if prompt := st.chat_input("Pregunta"):
         if prompt:
             # Obtener respuesta del modelo
             st.session_state.chat_memory.add_user_message(prompt)
-            with st.chat_message("user"):
-                st.markdown(prompt)
+            with chat_container:
+                with st.chat_message("user"):
+                    st.markdown(prompt)
 
-            with st.status("Consultando información ..." , expanded=True) as status:
+                with st.status("Consultando información ..." , expanded=True) as status:
                     
-                response = get_response(prompt,st.session_state["thread_id"])
-                st.session_state.chat_memory.add_ai_message(response)
+                    response = get_response(prompt,st.session_state["thread_id"])
+                    st.session_state.chat_memory.add_ai_message(response)
             # Mostrar la respuesta
-                with st.chat_message("assistant"):
-                    st.markdown(response) 
-                status.update(label="",state="complete")      
+                    with st.chat_message("assistant"):
+                        st.markdown(response) 
+                        status.update(label="",state="complete")      
 
             
             # Limpiar el campo de entrada
