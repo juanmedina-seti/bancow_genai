6b9f009eb435ac6714746dd1aeceda97bba01fe5
Juan Medina <juanmedina@seti.com.co>
2024-12-10 13:52:54 -0500

    run 80

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m streamlit run src/QACierreCaliMemV2/principal.py --server.port 443 --server.address 0.0.0.0
+python -m streamlit run src/QACierreCaliMemV2/principal.py --server.port 8000 --server.address 0.0.0.0
